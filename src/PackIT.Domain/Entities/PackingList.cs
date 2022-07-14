c5360d62fff52a6c06a30167a40b2045be2910c2
muratilgun <muratilgun34@gmail.com>
2022-07-15 02:00:37 +0300

    Entity & Value Object

---
+++ src/PackIT.Domain/Entities/PackingList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PackIT.Domain.Entities
+{
+    public class PackingList
+    {
+        public Guid Id { get; private set; }
+
+        private string _name;
+        private string _localization;
+
+        internal PackingList(Guid id, string name, string localization)
+        {
+            Id = id;
+            _name = name;
+            _localization = localization;
+        }
+    }
+}
---
+++ src/PackIT.Domain/ValueObjects/PackingListName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PackIT.Domain.ValueObjects
+{
+    public record PackingListName
+    {
+        public string Value { get; }
+
+        public PackingListName(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                //throw  new Exception()
+            }
+            Value = value;
+        }
+
+    }
+}
