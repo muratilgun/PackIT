63db3f2e0277d14909bedb9eea26a06d74045660
muratilgun <muratilgun34@gmail.com>
2022-07-19 00:11:41 +0300

    Create IPackingListRepository.cs

---
+++ src/PackIT.Domain/Repositories/IPackingListRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using PackIT.Domain.Entities;
+using PackIT.Domain.ValueObjects;
+
+namespace PackIT.Domain.Repositories
+{
+    public interface IPackingListRepository
+    {
+        Task<PackingList> GetAsync(PackingListId id);
+        Task<PackingList> AddAsync(PackingListId packingList);
+        Task<PackingList> UpdateAsync(PackingListId packingList);
+        Task<PackingList> DeleteAsync(PackingListId packingList);
+    }
+}
