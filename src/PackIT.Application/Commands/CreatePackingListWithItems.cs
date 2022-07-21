966fa0af7d300c2fa4a8c838d1795a268d96b146
muratilgun <muratilgun34@gmail.com>
2022-07-21 18:43:36 +0300

    Create CreatePackingListWithItems.cs

---
+++ src/PackIT.Application/Commands/CreatePackingListWithItems.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using PackIT.Domain.Consts;
+using PackIT.Domain.ValueObjects;
+using PackIT.Shared.Abstractions.Commands;
+
+namespace PackIT.Application.Commands
+{
+    public record CreatePackingListWithItems(Guid Id, string Name,ushort Days,Gender Gender,
+        LocalizationWriteModel Localization) : ICommand;
+
+    public record LocalizationWriteModel(string City, string Country);
+}
