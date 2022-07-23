e7c9de70ab509387bf3cd6bf6010d3eb60813472
muratilgun <muratilgun34@gmail.com>
2022-07-23 23:00:12 +0300

    Added Commands

---
+++ src/PackIT.Application/Commands/RemovePackingItem.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using PackIT.Shared.Abstractions.Commands;
+
+namespace PackIT.Application.Commands
+{
+    public record RemovePackingItem(Guid PackingListId, string Name) : ICommand;
+
+}
