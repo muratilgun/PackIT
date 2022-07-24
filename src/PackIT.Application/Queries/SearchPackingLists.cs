098c03b939563b97037dd34cd34bc5093ff19d04
muratilgun <muratilgun34@gmail.com>
2022-07-24 23:03:39 +0300

    Queries Fixed

---
+++ src/PackIT.Application/Queries/SearchPackingLists.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using PackIT.Application.DTO;
+using PackIT.Shared.Abstractions.Queries;
+
+namespace PackIT.Application.Queries
+{
+    public class SearchPackingLists :IQuery<IEnumerable<PackingListDto>>
+    {
+        public string SearchPhrase { get; set; }
+    }
+}
