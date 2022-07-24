c2a4e753d9ef4d62cd1afb2245565d038b04256c
muratilgun <muratilgun34@gmail.com>
2022-07-24 20:37:43 +0300

    Queries
    Query / Query Handler / Query Dispatcher definitions

---
+++ src/PackIT.Shared.Abstractions/Queries/IQueryHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace PackIT.Shared.Abstractions.Queries
+{
+    public interface IQueryHandler<in TQuery,TResult> where TQuery : class,IQuery<TResult>
+    {
+        Task<TResult> HandleAsync(TQuery query);
+
+    }
+}
---
+++ src/PackIT.Shared/Queries/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using PackIT.Shared.Abstractions.Commands;
+using PackIT.Shared.Abstractions.Queries;
+using PackIT.Shared.Commands;
+
+namespace PackIT.Shared.Queries
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddCommands(this IServiceCollection services)
+        {
+            var assembly = Assembly.GetCallingAssembly();
+            services.AddSingleton<IQueryDispatcher, InMemoryQueryDispatcher>();
+            services.Scan(s =>
+                s.FromAssemblies(assembly)
+                    .AddClasses(c => c.AssignableTo(typeof(IQueryHandler<,>)))
+                    .AsImplementedInterfaces()
+                    .WithScopedLifetime());
+            return services;
+        }
+    }
+}
