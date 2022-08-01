658c2f62558a56d2d6376b72acbde05aa5605889
muratilgun <muratilgun34@gmail.com>
2022-08-01 22:38:55 +0300

    Update PackingListTests.cs

--- tests/PackIT.UnitTest/Domain/PackingListTests.cs
+++ tests/PackIT.UnitTest/Domain/PackingListTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using PackIT.Domain.Entities;
+using PackIT.Domain.Events;
 using PackIT.Domain.Exceptions;
 using PackIT.Domain.Factories;
 using PackIT.Domain.Policies;
@@ -17,23 +18,35 @@ namespace PackIT.UnitTest.Domain
         {
             //Arrange
             var packingList = GetPackingList();
-            packingList.AddItem(new PackingItem("item 1",1));
+            packingList.AddItem(new PackingItem("item 1", 1));
             //Act
-            var exception = Record.Exception(()=>packingList.AddItem(new PackingItem("item 1",1)));
+            var exception = Record.Exception(() => packingList.AddItem(new PackingItem("item 1", 1)));
             exception.ShouldNotBeNull();
             exception.ShouldBeOfType<PackingItemAlreadyExistsException>();
-
         }
 
-      
+        [Fact]
+        public void AddItem_Adds_PackingItemAdded_Domain_Event_On_Success()
+        {
+            var packingList = GetPackingList();
+            
+            //ACT
+            var exception = Record.Exception(() => packingList.AddItem(new PackingItem("Item 1", 1)));
+            exception.ShouldBeNull();
+            packingList.Events.Count().ShouldBe(1);
+            var @event = packingList.Events.FirstOrDefault();
+            @event.ShouldBeOfType<PackingItemAdded>();
+        }
 
         #region ARRANGE
+
         private PackingList GetPackingList()
         {
             var packingList = _factory.Create(Guid.NewGuid(), "MyList", Localization.Create("Warsaw,Poland"));
             packingList.ClearEvents();
             return packingList;
         }
+
         private readonly IPackingListFactory _factory;
 
         public PackingListTests()
